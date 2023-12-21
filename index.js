b85af79c39c4a7ca8f10f8d27e8dd2382bb59a8b
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2023-12-21 09:01:55 +0600

    user-added-db

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ async function run() {
         await client.connect();
         const usersCollection = client.db("task-management").collection("users");
 
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
